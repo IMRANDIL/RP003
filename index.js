016f632dc199fbbf89b32935d183e03d61687479
ALI IMRAN ADIL <aliimranadil2@gmail.com>
2021-04-03 20:45:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var preloader = document.getElementById('building');
+
+function myfunct(){
+    preloader.style.display= 'none';
+}
+
+
+
+
+
+
+
+
+let pop = document.getElementById('popup');
+
+
+pop.addEventListener('click', function(){
+    alert("Chill! It's getting ready :) ")
+})
+
+
+
+
+
+
+// pooping
+
+
+let poop = document.getElementById('poopup');
+
+
+poop.addEventListener('click', function(){
+    alert("Chill! It's getting ready :) ")
+})
+
